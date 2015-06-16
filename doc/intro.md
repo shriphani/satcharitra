4971ac3bf916304e1eca54a6bcd50e1d1c0ff847
Shriphani Palakodety <shriphanip@gmail.com>
2015-06-15 19:08:57 -0700

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to satcharitra
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/satcharitra/core_test.clj
@@ -0,0 +1,7 @@
+(ns satcharitra.core-test
+  (:require [clojure.test :refer :all]
+            [satcharitra.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
