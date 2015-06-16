811a0a82a8a1e9bd4275241042276d01a6ead372
Shriphani Palakodety <shriphanip@gmail.com>
2015-06-16 01:39:25 -0700

    grabbed corpus

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject satcharitra "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Convert the Satcharitra to a pleasant PDF experience"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "1.1.2"]
+                 [enlive "1.1.5"]
+                 [org.bovinegenius/exploding-fish "0.3.4"]])
--- src/satcharitra/core.clj
+++ src/satcharitra/core.clj
@@ -1,6 +1,40 @@
-(ns satcharitra.core)
+(ns satcharitra.core
+  (:require [clj-http.client :as client]
+            [clojure.java.io :as io]
+            [clojure.pprint :refer [pprint]]
+            [net.cgrand.enlive-html :as html]
+            [org.bovinegenius.exploding-fish :as uri]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn grab-chapter-links
+  [root-link]
+  (let [chapter-tags (-> root-link
+                         (java.net.URL.)
+                         html/html-resource
+                         (html/select [:table :tbody :tr :td :a]))
+        links
+        (map
+         (fn [t]
+           (->> t :attrs :href (uri/resolve-uri root-link)))
+         chapter-tags)
+
+        text
+        (map html/text chapter-tags)]
+    (map vector links text)))
+
+(defn process-chapter
+  [chapter-link]
+  (-> chapter-link
+      client/get
+      :body))
+
+(defn grab-dataset
+  [root-link corpus-file]
+  (let [links-text (grab-chapter-links root-link)]
+    (with-open [wrtr (io/writer corpus-file)]
+      (pprint
+       (reduce
+        (fn [acc [link text]]
+          (merge acc {text (process-chapter link)}))
+        {}
+        links-text)
+       wrtr))))
